34e0ec11867b9b7eefdda09b8a048ad82641dc1e
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-01-02 23:09:02 -0300

    feat: add update method in AddressService

--- src/main/java/com/address_api/address_api/service/AddressService.java
+++ src/main/java/com/address_api/address_api/service/AddressService.java
@@ -1,10 +1,13 @@
 package com.address_api.address_api.service;
 
 import com.address_api.address_api.domain.Address;
+import com.address_api.address_api.dto.AddressDTO;
 import com.address_api.address_api.repository.AddressRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AddressService {
@@ -25,5 +28,24 @@ public class AddressService {
     var addressList = this.addressRepository.findAll();
 
     return addressList;
+  }
+
+  public String update(AddressDTO request, UUID id) {
+    Optional<Address> existingAddress = this.addressRepository.findById(id);
+
+    if(existingAddress.isPresent()) {
+      Address address = existingAddress.get();
+      if(request.country() != null) address.setCountry(request.country());
+      if(request.state() != null) address.setState(request.state());
+      if(request.city() != null) address.setCity(request.city());
+      if(request.neighborhood() != null) address.setNeighborhood(request.neighborhood());
+      if(request.postalCode() != null) address.setPostalCode(request.postalCode());
+      if(request.street() != null) address.setStreet(request.street());
+      if(request.number() != null) address.setNumber(request.number());
+
+      this.addressRepository.save(address);
+    }
+
+    return "Endereço atualizado!!";
   }
 }
